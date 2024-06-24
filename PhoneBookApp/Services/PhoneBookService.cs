75ccd53b40a7fdea89c27de6eac7ed7f7878e370
Dilshodbek Abdullayev <abdullayevdilshodbek1998@gmail.com>
2024-06-24 20:41:20 +0500

    Create added

--- PhoneBookApp/Services/PhoneBookService.cs
+++ PhoneBookApp/Services/PhoneBookService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PhoneBookApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,32 @@ using System.Threading.Tasks;
 
 namespace PhoneBookApp.Services
 {
-    internal class PhoneBookService
+    class PhoneBookService
     {
+         public static List<PhoneBookModel> phoneContact = new List<PhoneBookModel>();
+        public static void ShowAllContact()
+        {
+            if (phoneContact.Count == 0)
+            {
+                Console.WriteLine("Your Contact is Empty!");
+            }
+            else
+            {
+                for (int i = 0; i < phoneContact.Count; i++)
+                {
+                    Console.WriteLine($"{i+1}. Name {phoneContact[i].authorName} Phonne number is {phoneContact[i].phoneNumber}");
+                }
+            }
+        }
+        public static void AddContact()
+        {
+            Console.Write("Enter name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter phone number");
+            string number = Console.ReadLine();
+
+            phoneContact.Add(new PhoneBookModel { phoneNumber = number, authorName = name });
+            Console.WriteLine("Contact succesfully added");
+        }
     }
 }
