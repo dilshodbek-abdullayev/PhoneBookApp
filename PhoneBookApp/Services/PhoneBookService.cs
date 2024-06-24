9b31e72d374a20072c324eb74239c517ba94bd7f
Dilshodbek Abdullayev <abdullayevdilshodbek1998@gmail.com>
2024-06-24 20:58:15 +0500

    Create UpdateByIdContact method

--- PhoneBookApp/Services/PhoneBookService.cs
+++ PhoneBookApp/Services/PhoneBookService.cs
@@ -1,15 +1,12 @@
 ﻿using PhoneBookApp.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PhoneBookApp.Services
 {
     class PhoneBookService
     {
-         public static List<PhoneBookModel> phoneContact = new List<PhoneBookModel>();
+        public static List<PhoneBookModel> phoneContact = new List<PhoneBookModel>();
         public static void ShowAllContact()
         {
             if (phoneContact.Count == 0)
@@ -20,7 +17,7 @@ namespace PhoneBookApp.Services
             {
                 for (int i = 0; i < phoneContact.Count; i++)
                 {
-                    Console.WriteLine($"{i+1}. Name {phoneContact[i].authorName} Phonne number is {phoneContact[i].phoneNumber}");
+                    Console.WriteLine($"{i + 1}. Name {phoneContact[i].authorName} Phonne number is {phoneContact[i].phoneNumber}");
                 }
             }
         }
@@ -34,5 +31,26 @@ namespace PhoneBookApp.Services
             phoneContact.Add(new PhoneBookModel { phoneNumber = number, authorName = name });
             Console.WriteLine("Contact succesfully added");
         }
+        public static void UpdateByIdContact()
+        {
+            ShowAllContact();
+            Console.Write("Select the ID you want to change");
+            int inputId = Convert.ToInt32(Console.ReadLine());
+            if (inputId > 0 && inputId<= phoneContact.Count) 
+            {
+                inputId--;
+                Console.Write("Enter name: ");
+                phoneContact[inputId].authorName = Console.ReadLine();
+                Console.Write("Enter number: ");
+                phoneContact[inputId].phoneNumber = Console.ReadLine();
+
+                Console.WriteLine("Contact successfully updated");
+            }
+            else
+            {
+                Console.WriteLine("Invalid ID number");
+            }
+        }
+
     }
 }
